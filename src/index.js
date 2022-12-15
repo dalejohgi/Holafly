fbeb267cc532115b18d7fa08567e28fc9ff3feef
David Hincapie <dalejohgi.tech@gmail.com>
2022-12-15 00:18:12 -0500

    refactor (app index):  Refactor app entrypoint

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
-
-
-const bodyParser = require('body-parser');
-
 const createServer = require('./server');
+const db = require('./db');
+const services = require('./services');
 
-const app = require('./app');
+const app = {
+  db,
+  services,
+};
 
 async function start() {
-	const server = await createServer(app);
-
-  server.get('/', (req, res) => {
-    res.send('Welcome to Holafly\'s Technical test!');
-  });
-
-	// Start the GraphQL server
+  const server = await createServer(app);
+	
+  // Start the GraphQL server
   const port = process.env.PORT || 4567;
-	server.listen(port , () => {
-		// eslint-disable-next-line no-console
-		console.log(`Server is running on port: ${port}`);
-	});
-
+  server.listen(port, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Server is running on port: ${port}`);
+  });
 }
 
 start();
