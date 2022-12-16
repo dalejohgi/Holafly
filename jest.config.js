eea37e68ce84386865174d462c1a8be81b8d1b3b
David Hincapie <dalejohgi.tech@gmail.com>
2022-12-15 21:00:15 -0500

    chore (Config Files): Update prettier file, and jest config

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,4 @@ module.exports = {
   testEnvironment: 'node',
   verbose: true,
   testPathIgnorePatterns: ['<rootDir>/(lib|build|docs|node_modules|coverage)/'],
-  coveragePathIgnorePatterns: [
-    '<rootDir>/(node_modules|src/(config|models|utils))/',
-    '<rootDir>/(index|jest.config).js',
-  ],
-  collectCoverage: true,
-  //coverageReporters: ['lcov'],
 };
