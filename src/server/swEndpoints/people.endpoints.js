7e11314d85f91d375d6c3a660c9b383ce4dd10d8
David Hincapie <dalejohgi.tech@gmail.com>
2023-01-08 19:08:10 -0500

    refactor (weightOnPlanet):  Refaaactor controller response

--- src/server/swEndpoints/people.endpoints.js
+++ src/server/swEndpoints/people.endpoints.js
@@ -1,61 +1,13 @@
-const {
-  peopleServices,
-  planetServices,
-} = require('../../services');
+const { peopleServices, planetServices } = require('../../services');
 
 const applyPeopleEndpoints = (server, app) => {
   server.get('/hfswapi/people/weightOnRandomPlanet', async (req, res) => {
     const randomPerson = await peopleServices.getRandomPerson(app);
     const randomPlanet = await planetServices.getRandomPlanet(app);
 
-    const isTheHomePlanet = await planetServices.validateHomePlanet(
-      randomPerson.homeworld,
-      randomPlanet.name,
-    );
-
-    if (isTheHomePlanet) {
-      return res.status(409).send({
-        status: 'ERROR',
-        message: `Trying to calculate the weight for ${randomPerson.name} on ${randomPlanet.name} but it seems this is it's homeworld.`,
-        person: {
-          name: randomPerson.name,
-          mass: randomPerson.mass,
-          homeworld: randomPerson.homeworld,
-        },
-        planet: {
-          name: randomPlanet.name,
-          gravity: randomPlanet.gravity,
-          url: randomPlanet.url,
-        },
-      });
-    }
-
-    const gravity = randomPlanet.gravity.split(' ');
-    if (isNaN(gravity[0]) || isNaN(randomPerson.mass)) {
-      return res.status(409).send({
-        status: 'ERROR',
-        message: `Unkown values - Plannet's gravity and person's mass must be valid numbers`,
-        person: {
-          name: randomPerson.name,
-          mass: randomPerson.mass,
-          homeworld: randomPerson.homeworld,
-        },
-        planet: {
-          name: randomPlanet.name,
-          gravity: randomPlanet.gravity,
-          url: randomPlanet.url,
-        },
-      });
-    }
-    const weight = planetServices.getWeightOnPlanet(
-      randomPerson.mass,
-      +gravity[0],
-    );
-    return res.status(200).send({
+    const response = {
       status: 'OK',
-      message: `The weight for ${randomPerson.name} on ${
-        randomPlanet.name
-      } is ${weight.toFixed(2)} kg.`,
+      message: '',
       person: {
         name: randomPerson.name,
         mass: randomPerson.mass,
@@ -66,6 +18,46 @@ const applyPeopleEndpoints = (server, app) => {
         gravity: randomPlanet.gravity,
         url: randomPlanet.url,
       },
+    };
+
+    const isTheHomePlanet = await planetServices.validateHomePlanet(
+      randomPerson.homeworld,
+      randomPlanet.name,
+    );
+
+    if (isTheHomePlanet) {
+      return res.status(409).send({
+        ...response,
+        status: 'ERROR',
+        message: `Can't calculate the weight for ${randomPerson.name} on ${randomPlanet.name} since this is it's homeworld.`,
+      });
+    }
+
+    const gravity = randomPlanet.gravity.split(' ');
+
+    if (isNaN(gravity[0])) {
+      return res.status(200).send({
+        ...response,
+        message: `The weight of ${randomPerson.name} on ${randomPlanet.name} is unkown until we get more info about the gravity on this plannet.`,
+      });
+    }
+
+    if (isNaN(randomPerson.mass)) {
+      return res.status(200).send({
+        ...response,
+        message: `The weight of ${randomPerson.name} on ${randomPlanet.name} is unkown until we get more info about this person's mass on its homeworld.`,
+      });
+    }
+
+    const weight = planetServices.getWeightOnPlanet(
+      randomPerson.mass,
+      +gravity[0],
+    );
+    return res.status(200).send({
+      ...response,
+      message: `The weight for ${randomPerson.name} on ${
+        randomPlanet.name
+      } is ${weight.toFixed(2)} kg.`,
       WeightOnPlanet: weight.toFixed(2),
     });
   });
